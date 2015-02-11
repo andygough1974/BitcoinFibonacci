193d082c2255033866468ecfc027373b77e18f0d
BitBullExchange <mrorbita@mrOrbitas-MacBook-Pro.local>
2015-02-11 18:02:19 +0400

    updated

--- fibonachiFunction.js
+++ fibonachiFunction.js
@@ -1,9 +1,12 @@
 function fibonachiNumber(n) {
-    if (n < 2) {
-        return 1;
-    } else {
-        return fibonachiNumber(n - 2) + fibonachiNumber(n - 1);
-    }
+//    if (n < 2) {
+//        return 1;
+//    } else {
+//        return fibonachiNumber(n - 2) + fibonachiNumber(n - 1);
+//    }
+
+    
+
 }
 
 
