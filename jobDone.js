92e258b6d68271cafa18b3a57ca0da036827cb29
mrAlex <mrorbita@gmail.com>
2015-03-05 09:28:14 +0300

    Update jobDone.js

--- jobDone.js
+++ jobDone.js
@@ -3,44 +3,44 @@
 function jobDone() {
 
         return true;
-    // this function are checking all unit tests and return true if job is done
-    var expectedResult = [
-        {
-            number : 0,
-            value : 1
-        },
-        {
-            number : 5,
-            value : 8
-        },
-        {
-            number : 10,
-            value : 89
-        },
-        {
-            number : 15,
-            value : 987
-        },
-        {
-            number : 20,
-            value : 10946
-        },
-        {
-            number : 25,
-            value : 121393
-        }
-    ];
+//     // this function are checking all unit tests and return true if job is done
+//     var expectedResult = [
+//         {
+//             number : 0,
+//             value : 1
+//         },
+//         {
+//             number : 5,
+//             value : 8
+//         },
+//         {
+//             number : 10,
+//             value : 89
+//         },
+//         {
+//             number : 15,
+//             value : 987
+//         },
+//         {
+//             number : 20,
+//             value : 10946
+//         },
+//         {
+//             number : 25,
+//             value : 121393
+//         }
+//     ];
 
-    var unitTestResult = true;
-    for (var index = 0; index < expectedResult.length; index++) {
-        var actual = _fibonachiFunction(expectedResult[index].number;
-        var expected = expectedResult[index].value;
-        var result = actual == expected;
-        if (result == false) {
-            unitTestResult = result;
-        }
-    }
-    return unitTestResult;
+//     var unitTestResult = true;
+//     for (var index = 0; index < expectedResult.length; index++) {
+//         var actual = _fibonachiFunction(expectedResult[index].number;
+//         var expected = expectedResult[index].value;
+//         var result = actual == expected;
+//         if (result == false) {
+//             unitTestResult = result;
+//         }
+//     }
+//     return unitTestResult;
 
 }
 
